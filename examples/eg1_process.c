793bc411e5e5cae6c747bd878fb90b4e7acb0216
zhuyoujun <zhuyoujun0513@163.com>
2016-01-04 03:24:11 -0800

    update

---
+++ examples/eg1_process.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<fcntl.h>
+
+int main()
+{
+	int pid = fork();
+	if(pid > 0)
+	{
+		printf("parent: chid = %d\n",pid);
+		pid = wait();
+		printf("chid %d is done\n", pid);
+	}
+	else if (pid == 0)
+	{
+		printf("chid exiting\n",pid);
+		exit(0);
+	}
+	else
+	{
+		printf("fork error\n");
+	}
+
+	return 0;
+}
